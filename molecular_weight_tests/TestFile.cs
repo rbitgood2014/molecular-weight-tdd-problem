94ed8213ca20d30fd19d23d4ef9b19b70e32df34
rbitgood2014 <rbitgood2014@fau.edu>
2016-05-20 23:09:38 -0400

    added unit test

---
+++ molecular_weight_tests/TestFile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+namespace molecular_weight_tests
+{
+    [TestClass]
+    public class TestFile
+    {
+        [TestMethod]
+        public void TestElementFileExists()
+        {
+            Assert.IsTrue(File.Exists("c:/users/robert/documents/visual studio 2015/Projects/molecular_weight/molecular_weight/elements.csv"));
+        }
+    }
+}
