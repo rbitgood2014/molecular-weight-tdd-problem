b5e05f46e87776f860b158b3c7e79dd8c122eeb8
rbitgood2014 <rbitgood2014@fau.edu>
2016-05-20 23:49:07 -0400

    added element class

---
+++ molecular_weight_tests/Test_Element.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace molecular_weight_tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestHydrogenElement()
+        {   
+            Element hydrogen = new Element("Hydrogen",1,"H",1.01);
+            Assert.Equals("Hydrogen",hydrogen.getElementName());
+            
+        }
+    }
+}
